fa6c6877d2235b6a10f122e844a1a78f43746a68
bchizi <leonbichanga@gmail.com>
2019-11-07 12:32:24 +0300

    configured the setings to include my templates

--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('welcome to my foto ops')
+    return render(request,'welcome.html')
+
