29d23946b2626bde9b866dac72aef3b030a6927b
bchizi <leonbichanga@gmail.com>
2019-11-07 16:01:04 +0300

    made and edited the navbar

--- foto_ops/settings.py
+++ foto_ops/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #'photo',
+    'bootstrap4',
+    'photo.apps.PhotoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photo.apps.PhotoConfig'
     
 ]
 
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, "static"),
+]
